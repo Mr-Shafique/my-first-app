4d6a497997410401e9e470b79d0ec72307839738
Muhammad Shafique <muhammadshafiquenarejo@gmail.com>
2024-12-12 14:17:23 +0500

    added modal

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Modal } from "antd";
+
 import {
   MDBCard,
   MDBCardBody,
@@ -8,40 +10,78 @@ import {
 } from "mdb-react-ui-kit";
 
 export default function Dashboard() {
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const items = [
-    { id: 1, name: "Alert Notifiction", description: "Stay informed with instant alerts for any security breaches or suspicious activity." },
-    { id: 2, name: "Live Camera View", description: "Monitor real-time footage from your security cameras anywhere, anytime." },
-    { id: 3, name: "Add Restricted Persons", description: "Enhance security by adding individuals to a restricted list for immediate detection." },
-    { id: 4, name: "Alert History", description: "Review past notifications and incidents for better security management." },
-    { id: 4, name: "Restricted persons list ", description: "Maintain a detailed list of flagged individuals for efficient monitoring and action." },
+    {
+      id: 1,
+      name: "Alert Notifiction",
+      description:
+        "Stay informed with instant alerts for any security breaches or suspicious activity.",
+    },
+    {
+      id: 2,
+      name: "Live Camera View",
+      description:
+        "Monitor real-time footage from your security cameras anywhere, anytime.",
+    },
+    {
+      id: 3,
+      name: "Add Restricted Persons",
+      description:
+        "Enhance security by adding individuals to a restricted list for immediate detection.",
+    },
+    {
+      id: 4,
+      name: "Alert History",
+      description:
+        "Review past notifications and incidents for better security management.",
+    },
+    {
+      id: 4,
+      name: "Restricted persons list ",
+      description:
+        "Maintain a detailed list of flagged individuals for efficient monitoring and action.",
+    },
   ];
 
   return (
     <>
-    <div className="d-flex flex-wrap justify-content-center ">
-      {items.map((item) => (
+      <div className="d-flex flex-wrap justify-content-center ">
+        {items.map((item) => (
           <div className="m-1 w-25">
-        <MDBCard>
-          <MDBCardBody>
-            <MDBCardTitle>{item.name}</MDBCardTitle>
-            <MDBCardText>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </MDBCardText>
-            <MDBBtn>Button</MDBBtn>
-          </MDBCardBody>
-        </MDBCard>
-        </div>
-      ))}
+            <MDBCard>
+              <MDBCardBody>
+                <MDBCardTitle>{item.name}</MDBCardTitle>
+                <MDBCardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </MDBCardText>
+                <MDBBtn onClick={showModal}>Button</MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+        ))}
       </div>
-
-
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </>
   );
 }
-
-
-
-
